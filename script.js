37f849b30342db7aea9f2942c3b70fae6275a579
umer-rwx <umertariqsocial@gmail.com>
2024-07-02 20:06:17 +0500

    First working model with DOM. Lacks polish. But perfectly functional.

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 "use strict";
 
+let turnNumber = 0;
 let humanScore = 0;
 let computerScore = 0;
+
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
+
+rockBtn.addEventListener( "click" , () => playRound("rock", getComputerChoice() ) );
+paperBtn.addEventListener( "click" , () => playRound("paper", getComputerChoice() ) );
+scissorsBtn.addEventListener( "click" , () => playRound("scissors", getComputerChoice() ) );
+
+function printInResult(str) {
+    const resultDiv = document.getElementById("result");
+    const output = document.createElement("p");
+
+    output.textContent = "> " + str;
+    resultDiv.prepend(output);
+}
 
 function getComputerChoice() {
     let ran = Math.floor( Math.random() * 3 );
@@ -14,66 +31,45 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let choice = prompt("Rock, paper, or scissors?");
-    return choice.toLowerCase();
-}
-
 function playRound(humanChoice, computerChoice) {
-    humanChoice = getHumanChoice();
     computerChoice = getComputerChoice();
-    console.log("Computer chose " + computerChoice);
+    //printInResult("Computer chose " + computerChoice);
 
     switch (humanChoice) {
         case "rock":
             if (computerChoice === "rock") {
-                console.log("Tie");
+                printInResult("Tie");
             } else if (computerChoice === "paper") {
-                console.log("Computer gets a point.");
+                printInResult("Computer gets a point.");
                 computerScore++;
             } else {
-                console.log("Human gets a point.")
+                printInResult("Human gets a point.")
                 humanScore++;
             }
             break;
         case "paper":
             if (computerChoice === "rock") {
-                console.log("Human gets a point.")
+                printInResult("Human gets a point.")
                 humanScore++;
             } else if (computerChoice === "paper") {
-                console.log("Tie");
+                printInResult("Tie");
             } else {
-                console.log("Computer gets a point.");
+                printInResult("Computer gets a point.");
                 computerScore++;
             }
             break;
         case "scissors":
             if (computerChoice === "rock") {
-                console.log("Computer gets a point.");
+                printInResult("Computer gets a point.");
                 computerScore++;
             } else if (computerChoice === "paper") {
-                console.log("Human gets a point.")
+                printInResult("Human gets a point.")
                 humanScore++;
             } else {
-                console.log("Tie");
+                printInResult("Tie");
             }
             break;
         default:
-            console.log("Sorry, I don't understand");
+            printInResult("Sorry, I don't understand");
     }
 }
-
-function playGame() {
-    for (let step = 1; step <= 5; step++) {
-        playRound();
-    }
-    console.log("Human Score: " + humanScore);
-    console.log("Computer Score: " + computerScore);
-    if (computerScore > humanScore) {
-        console.log("Computer wins!");
-    } else {
-        console.log("Human wins!");
-    }
-}
-
-playGame();
