1fd6870160d8d50510729739a17f76fd80c3a1aa
umer-rwx <umertariqsocial@gmail.com>
2024-07-02 20:56:55 +0500

    Polished it up. Ready to go!

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-"use strict";
-
 let turnNumber = 0;
 let humanScore = 0;
 let computerScore = 0;
@@ -7,17 +5,26 @@ let computerScore = 0;
 const rockBtn = document.getElementById("rock");
 const paperBtn = document.getElementById("paper");
 const scissorsBtn = document.getElementById("scissors");
+const scoreCard = document.getElementById("score");
 
 rockBtn.addEventListener( "click" , () => playRound("rock", getComputerChoice() ) );
 paperBtn.addEventListener( "click" , () => playRound("paper", getComputerChoice() ) );
 scissorsBtn.addEventListener( "click" , () => playRound("scissors", getComputerChoice() ) );
 
+
 function printInResult(str) {
     const resultDiv = document.getElementById("result");
     const output = document.createElement("p");
 
-    output.textContent = "> " + str;
+    output.textContent = `[${turnNumber}] -> ` + str;
     resultDiv.prepend(output);
+}
+
+function setMaxResultsShown(number) {
+    if (turnNumber > number) {
+        const oldResult = document.getElementById("result").lastChild;
+        oldResult.remove();
+    }
 }
 
 function getComputerChoice() {
@@ -32,44 +39,46 @@ function getComputerChoice() {
 }
 
 function playRound(humanChoice, computerChoice) {
+    turnNumber++;
     computerChoice = getComputerChoice();
-    //printInResult("Computer chose " + computerChoice);
 
     switch (humanChoice) {
         case "rock":
             if (computerChoice === "rock") {
-                printInResult("Tie");
+                printInResult("Human chose " + humanChoice + ". Computer chose " + computerChoice + ". Tie");
             } else if (computerChoice === "paper") {
-                printInResult("Computer gets a point.");
+                printInResult("Human chose " + humanChoice + ". Computer chose " + computerChoice + ". Computer gets a point.");
                 computerScore++;
             } else {
-                printInResult("Human gets a point.")
+                printInResult("Human chose " + humanChoice + ". Computer chose " + computerChoice + ". Human gets a point.")
                 humanScore++;
             }
             break;
         case "paper":
             if (computerChoice === "rock") {
-                printInResult("Human gets a point.")
+                printInResult("Human chose " + humanChoice + ". Computer chose " + computerChoice + " Human gets a point.")
                 humanScore++;
             } else if (computerChoice === "paper") {
-                printInResult("Tie");
+                printInResult("Human chose " + humanChoice + ". Computer chose " + computerChoice + ". Tie");
             } else {
-                printInResult("Computer gets a point.");
+                printInResult("Human chose " + humanChoice + ". Computer chose " + computerChoice + ". Computer gets a point.");
                 computerScore++;
             }
             break;
         case "scissors":
             if (computerChoice === "rock") {
-                printInResult("Computer gets a point.");
+                printInResult("Human chose " + humanChoice + ". Computer chose " + computerChoice + ". Computer gets a point.");
                 computerScore++;
             } else if (computerChoice === "paper") {
-                printInResult("Human gets a point.")
+                printInResult("Human chose " + humanChoice + ". Computer chose " + computerChoice + ". Human gets a point.")
                 humanScore++;
             } else {
-                printInResult("Tie");
+                printInResult("Human chose " + humanChoice + ". Computer chose " + computerChoice + ". Tie");
             }
             break;
         default:
             printInResult("Sorry, I don't understand");
     }
+    scoreCard.textContent = "Human Score: " + humanScore + "\n" + ". Computer Score: " + computerScore;
+    setMaxResultsShown(20);
 }
